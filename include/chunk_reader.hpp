86811391784c26393e4e7bc8cfa72d248f126670
rednax@ubuntu.ubuntu-domain <rednax@ubuntu.ubuntu-domain>
2010-04-24 14:08:48 +0200

    moar documentation

--- include/chunk_reader.hpp
+++ include/chunk_reader.hpp
@@ -1,3 +1,31 @@
+/*
+Copyright 2010 CowboyCoders. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY COWBOYCODERS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COWBOYCODERS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of CowboyCoders.
+*/
+
 #ifndef ___CHUNK_READER_H___
 #define ___CHUNK_READER_H___
 
@@ -6,50 +34,47 @@
 
 class chunk_reader {
 public:
-    /**
-     * Creates and initalizes a new chunk_reader.
-     * @param max_read Maximum size to return from a single request.
-     */
+   /**
+    * Creates and initalizes a new chunk_reader.
+    * @param max_read Maximum size to return from a single request.
+    */
     chunk_reader(size_t max_read);
 
     ~chunk_reader();
 
-    /**
-     * Read a not necessarily sequential list of chunks based on the
-     * supplied indices.
-
-     * @param size Chunk size
-     * @param indices Chunk indices to read
-     */
+   /**
+    * Read a not necessarily sequential list of chunks based on the
+    * supplied indices.
+    * @param filename The file to read from.
+    * @param size Chunk size.
+    * @param indices Chunk indices to read.
+    */
     bool read(std::string filename,
               size_t size,
               const std::vector<size_t>& indices);
 
-    /**
-     * Size of chunk
-     *
-     * @return a size_t describing the size
-     */
+   /**
+    * This function returns the size of a chunk.
+    * @return A size_t describing the size of a chunk.
+    */
     size_t chunk_size()
     { 
         return chunk_size_; 
     }
 
-    /**
-     * Size of read data
-     *
-     * @return a size_t describing how much data was read
-     */
+   /**
+    * This function returns how much data was read.
+    * @return A size_t describing how much data was rea.d
+    */
     size_t data_size()
     {
         return data_size_;
     }
     
-    /**
-     * Returns a pointer to the read data
-     *
-     * @return the pointer to the data
-     */
+   /**
+    * Returns a pointer to the read data.
+    * @return The pointer to the data.
+    */
     const char* chunk() { 
         return chunk_; 
     }
--- include/connection.hpp
+++ include/connection.hpp
@@ -1,3 +1,31 @@
+/*
+Copyright 2010 CowboyCoders. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY COWBOYCODERS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COWBOYCODERS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of CowboyCoders.
+*/
+
 #ifndef ___CONNECTION_H___ 
 #define ___CONNECTION_H___ 
 
@@ -9,29 +37,22 @@
 
 #include "chunk_reader.hpp"
 
-/**
- * Handles a single request.
- *
- * @param io_service boost::asio io_service
- * @param document_root Root path for documents.
- */
 class connection
     : public boost::enable_shared_from_this<connection>,
     private boost::noncopyable
 {
 public:
     /**
-     * Creates a new connection
+     * Creates a new connection.
      *
-     * @param io_service The socket which the connection will send data to
-     * @param document_root The root catalog where all the documents are
+     * @param io_service A worker for read jobs.
+     * @param document_root The root catalog where all the documents are.
      */
     explicit connection(boost::asio::io_service& io_service, const std::string& document_root);
 
     /**
-     * Returns the socket which the connection sends data to
-     *
-     * @return the tcp socket
+     * Returns the socket which the connection sends data to.
+     * @return The TCP socket.
      */
     boost::asio::ip::tcp::socket& socket()
     {
@@ -39,12 +60,12 @@ public:
     }
 
     /**
-     * Starts the connection
+     * Starts the connection.
      */
     void start();
 
     /**
-     * The maximum size of a request (in bytes)
+     * The maximum size of a request (in bytes).
      */
     const static int MAX_REQUEST_SIZE=1024*1024*10;
 
